18558418a668359acd5f2e9c71b742cc89e0b0c5
HuyQuyen2510 <huyquyenpham4@gmail.com>
2024-09-09 16:30:50 +0700

    Update 15.0

--- static/src/js/function_override.js
+++ static/src/js/function_override.js
@@ -1,55 +1,28 @@
 
-
 import { registry } from '@web/core/registry';
 import { UserMenu } from '@web/webclient/user_menu/user_menu';
 import { patch } from '@web/core/utils/patch';
 import { browser } from '@web/core/browser/browser';
-import { _lt } from "@web/core/l10n/translation";
 
-
-
-patch(UserMenu.prototype, {
+patch(UserMenu.prototype, 'custom_menu.menu_modifications', {
     setup() {
-
-        super.setup(...arguments);
-
+        this._super(...arguments);
         const userMenuRegistry = registry.category('user_menuitems');
         if (userMenuRegistry.get('documentation')) {
             userMenuRegistry.remove('documentation');
         }
-        if (userMenuRegistry.get('support')) {
-            userMenuRegistry.remove('support');
-        }
         if (userMenuRegistry.get('odoo_account')) {
             userMenuRegistry.remove('odoo_account');
         }
-       
-        
     },
 });
 
-
 function newDocumentationItem(env) {
-    
-    const documentationURL = 'https://docs.orissolutions.vn'; 
+    const documentationURL = 'https://docs.orissolutions.vn'; // URL tài liệu mới
     return {
         type: 'item',
         id: 'documentation',
-        description: 'Documentation',
-        href: documentationURL,
-        callback: () => {
-            browser.open(documentationURL, '_blank');
-        },
-        sequence: 10,
-    };
-}
-function newSupportItem(env) {
-    
-    const documentationURL = 'https://support.orissolutions.vn';
-    return {
-        type: 'item',
-        id: 'documentation',
-        description: 'Support',
+        description: env._t('Documentation'),
         href: documentationURL,
         callback: () => {
             browser.open(documentationURL, '_blank');
@@ -58,9 +31,4 @@ function newSupportItem(env) {
     };
 }
 
-registry
-.category('user_menuitems')
-.add('documentations', newDocumentationItem)
-.add('supports', newSupportItem);
-
-
+registry.category('user_menuitems').add('documentations', newDocumentationItem);
