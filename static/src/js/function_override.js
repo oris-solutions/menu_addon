db926d22d50ccfc0ad260faf1c098c77e83c0973
HuyQuyen2510 <huyquyenpham4@gmail.com>
2024-09-11 11:49:28 +0700

    update 17.0

--- static/src/js/function_override.js
+++ static/src/js/function_override.js
@@ -1,28 +1,27 @@
-
+/** @odoo-module **/
 
 import { registry } from '@web/core/registry';
 import { UserMenu } from '@web/webclient/user_menu/user_menu';
 import { patch } from '@web/core/utils/patch';
 import { browser } from '@web/core/browser/browser';
-import { _lt } from "@web/core/l10n/translation";
+import { _t } from "@web/core/l10n/translation";
 
 
 
 patch(UserMenu.prototype, {
     setup() {
-
         super.setup(...arguments);
 
         const userMenuRegistry = registry.category('user_menuitems');
-        if (userMenuRegistry.get('documentation')) {
+            userMenuRegistry.get('documentation');
             userMenuRegistry.remove('documentation');
-        }
-        if (userMenuRegistry.get('support')) {
+        
+            userMenuRegistry.get('support');
             userMenuRegistry.remove('support');
-        }
-        if (userMenuRegistry.get('odoo_account')) {
+        
+            userMenuRegistry.get('odoo_account');
             userMenuRegistry.remove('odoo_account');
-        }
+        
        
         
     },
@@ -31,33 +30,28 @@ patch(UserMenu.prototype, {
 
 function newDocumentationItem(env) {
     
-    const documentationURL = 'https://docs.orissolutions.vn'; 
     return {
         type: 'item',
         id: 'documentation',
-        description: 'Documentation',
-        href: documentationURL,
+        description: _t('Documentation'),
         callback: () => {
-            browser.open(documentationURL, '_blank');
+            browser.open('https://docs.orissolutions.vn', '_blank');
         },
         sequence: 10,
     };
 }
 function newSupportItem(env) {
     
-    const documentationURL = 'https://support.orissolutions.vn';
     return {
         type: 'item',
-        id: 'documentation',
-        description: 'Support',
-        href: documentationURL,
+        id: 'support',
+        description: _t('Support'),
         callback: () => {
-            browser.open(documentationURL, '_blank');
+            browser.open('https://orissolutions.vn', '_blank');
         },
         sequence: 10,
     };
 }
-
 registry
 .category('user_menuitems')
 .add('documentations', newDocumentationItem)
