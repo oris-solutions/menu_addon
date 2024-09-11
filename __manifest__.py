497583582951cdc1f345b10107442aa7a7eb5493
NanhOris <82574499+tom-oris@users.noreply.github.com>
2024-09-11 12:55:25 +0700

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'menu_addon/static/src/js/function_override.js',
+            'oris_menu_addon/static/src/js/function_override.js',
         ],
     },
     'license': 'LGPL-3',
