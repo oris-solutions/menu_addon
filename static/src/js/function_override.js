04950d3a6551e9f0e6ffbd8f5897fb6360c14b17
Phạm Huy Quyến <162394420+HuyQuyen2510@users.noreply.github.com>
2024-09-11 13:30:14 +0700

    Update function_override.js

--- static/src/js/function_override.js
+++ static/src/js/function_override.js
@@ -1,3 +1,4 @@
+/** @odoo-module **/
 
 import { registry } from '@web/core/registry';
 import { UserMenu } from '@web/webclient/user_menu/user_menu';
@@ -8,24 +9,43 @@ patch(UserMenu.prototype, 'custom_menu.menu_modifications', {
     setup() {
         this._super(...arguments);
         const userMenuRegistry = registry.category('user_menuitems');
-            userMenuRegistry.get('documentation');
-            userMenuRegistry.remove('documentation');
-            userMenuRegistry.get('odoo_account')
-            userMenuRegistry.remove('odoo_account');
+        userMenuRegistry.get('documentation');
+        userMenuRegistry.remove('documentation');
+        userMenuRegistry.get('support');
+        userMenuRegistry.remove('support');
+        userMenuRegistry.get('odoo_account') ;
+        userMenuRegistry.remove('odoo_account');
         
     },
 });
 
+
 function newDocumentationItem(env) {
+ 
     return {
         type: 'item',
         id: 'documentation',
         description: env._t('Documentation'),
         callback: () => {
-            browser.open('https://docs.orissolutions.vn', '_blank');
+            browser.open('https://doc.orissolutions.vn', '_blank');
         },
         sequence: 10,
     };
 }
 
-registry.category('user_menuitems').add('documentations', newDocumentationItem);
+function newSupportItem(env) {
+    return {
+        type: 'item',
+        id: 'support',
+        description: env._t('Support'),
+        callback: () => {
+            browser.open('https://support.orissolutions.vn', '_blank');
+        },
+        sequence: 10,
+    };
+}
+
+registry.category('user_menuitems')
+.add('documentations', newDocumentationItem)
+.add('supports', newSupportItem)
+;
